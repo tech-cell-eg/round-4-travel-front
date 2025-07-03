23aff6faa6c116199c1914f3ed40f50130b8df72
Mohamed Magdy <93835752+moamed018@users.noreply.github.com>
2025-07-03 19:55:04 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/round-4-travel-front">
       <App />
     </BrowserRouter>
   </StrictMode>
